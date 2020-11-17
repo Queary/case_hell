eb4cf19e1b2674ce10eaa01af9b25d8067768c99
Queary <72671469+Queary@users.noreply.github.com>
2020-11-18 02:22:51 +0300

    Update test.pyx

--- test.pyx
+++ test.pyx
@@ -4,3 +4,9 @@ cpdef int test(int x):
     for i in range(1, x+1):
         y *= i
     return y
+cdef float x = 0.5
+cdef float y = 0.5
+cdef float zx = 0.5
+cdef float xg = 0.5
+cdef float hyx = 0.5
+cdef float xy = 0.5
